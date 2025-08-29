a50882e78f79350ef161cdd3c150f11354f779f1
BMSales <bruno.m.sales03@gmail.com>
2025-08-28 21:04:51 -0300

    advancements

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 import numpy as np
 
 inputs = [1, 2, 3, 2.5]
-weights = [0.2, 0.8, -0.5, 1.0]
-bias = 2
 
-output = 0
-i = 0
+weights1 = [0.2, 0.8, -0.5, 1.0]
+weights2 = [0.5, -0.91, 0.26, -0.5]
+weights3 = [-0.26, -0.27, 0.17, 0.87]
 
-while i < len(inputs):
-    output += inputs[i] * weights[i]
-    i += 1
+weight_array = [weights1, weights2, weights3]
 
-output += bias
+bias1 = 2
+bias2 = 3
+bias3 = 0.5
+
+bias_array = [bias1, bias2, bias3]
+
+output = [0, 0, 0]
+i, j = 0, 0
+
+while j < len(weight_array):
+    while i < len(inputs):
+        output[j] += inputs[i] * weight_array[j][i]
+        i += 1
+    output[j] += bias_array[j]
+    i = 0
+    j += 1
+
 print(output)

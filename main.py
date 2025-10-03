31181889307477ca2915b4605a84947a54c277af
BMSales <bruno.m.sales03@gmail.com>
2025-10-03 08:35:26 -0300

    added loss calculation

--- main.py
+++ main.py
@@ -21,6 +21,25 @@ class Activation_Softmax:
         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
         self.output = probabilities
 
+class Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+
+class Loss_CategoricalCrossEntropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7)
+
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred_clipped * y_true, axis=1)
+
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
+
 X, y = spiral_data(100, 3)
 
 layer_1 = Layer_Dense(2, 3)
@@ -36,3 +55,8 @@ layer_2.forward(activation_1.output)
 activation_2.forward(layer_2.output)
 
 print(activation_2.output[:5])
+
+loss_function = Loss_CategoricalCrossEntropy()
+loss = loss_function.calculate(activation_2.output, y)
+
+print("Loss:", loss)
